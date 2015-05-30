0ba41b51aaabaa943ef86985c95ee5e1441ddd10
Harry He <hehaoqian@live.com>
2015-05-29 23:39:54 -0700

    fix naive

--- optimization_naive.c
+++ optimization_naive.c
@@ -2,15 +2,16 @@
 
 void optimization_naive(double* restrict result,
 		const double* restrict matrix1, const double* restrict matrix2) {
-	memset(result, 0, WIDTH*HEIGHT*sizeof(double));
 	for (int i = 0; i < WIDTH; i++)
 	{
 		for (int j = 0; j < HEIGHT; j++)
 		{
+			double temp = 0;
 			for (int k = 0; k < WIDTH; k++)
 			{
-				result[i*WIDTH+j] += matrix1[i*WIDTH+k]*matrix2[k*WIDTH+j];
+				temp += matrix1[i*WIDTH+k]*matrix2[k*WIDTH+j];
 			}
+			result[i*WIDTH+j] = temp;
 		}
 	}
 }
