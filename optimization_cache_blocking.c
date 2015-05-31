82cbff20b85fe7abf2445064742ae0b277687211
Harry He <hehaoqian@live.com>
2015-05-30 19:10:17 -0700

    fix cache blocking

--- optimization_cache_blocking.c
+++ optimization_cache_blocking.c
@@ -6,14 +6,15 @@
 void optimization_cache_blocking(double* restrict result,
 		const double* restrict matrix1, const double* restrict matrix2) {
 
+	memset(result, 0, WIDTH*HEIGHT*sizeof(double));
 	for (int w = 0; w < WIDTH; w += WIDTH_BLOCK) {
 		for (int h = 0; h < HEIGHT; h += HEIGHT_BLOCK) {
-			for (int i = w; i < w + WIDTH_BLOCK; i++)
+			for (int i = 0; i < WIDTH; i++)
 			{
 				for (int j = h; j < h + HEIGHT_BLOCK; j++)
 				{
-					double temp = 0;
-					for (int k = 0; k < WIDTH; k++)
+					double temp = result[i*WIDTH+j];
+					for (int k = w; k < w + WIDTH_BLOCK; k++)
 					{
 						temp += matrix1[i*WIDTH+k]*matrix2[k*WIDTH+j];
 					}
